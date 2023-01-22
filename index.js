826a7e880d620baa05fba79de2cdd17c312940a5
Muhammad Awais <awaisiqbal78692110@gmail.com>
2023-01-23 00:14:09 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 require("./config");
-const Products = require("./product");
+const Product = require("./product");
 
 
 const app = express();
+app.use(express.json());
 
-app.post("/create", (req, res) => {
-
+app.post("/create", async (req, res) => {
+    let data = new Product(req.body);
+    let result = await data.save();
+    console.log(req.body);
     res.send("Data has been save in DB");
 });
 app.listen(4500);
