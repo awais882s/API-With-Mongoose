1be7968b75a69d6215d2f35594a438ac069f962a
Muhammad Awais <awaisiqbal78692110@gmail.com>
2023-01-23 23:55:43 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://admin:admin@cluster0.cfg2fvd.mongodb.net/e-comm");                               
+mongoose.connect("mongodb+srv://admin:admin@cluster0.cfg2fvd.mongodb.net/e-comm");                                 
